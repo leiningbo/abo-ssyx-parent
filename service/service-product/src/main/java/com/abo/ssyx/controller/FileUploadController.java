1e9edd660115ec24b7d2ab7b0e69486d83d65e08
leiningbo <1071781349@qq.com>
2023-06-10 21:36:38 +0800

    controller增加/

--- service/service-product/src/main/java/com/abo/ssyx/controller/FileUploadController.java
+++ service/service-product/src/main/java/com/abo/ssyx/controller/FileUploadController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Api(tags = "文件上传接口")
 @RestController
-@RequestMapping("admin/product")
+@RequestMapping("/admin/product")
 //@CrossOrigin
 public class FileUploadController {
 
